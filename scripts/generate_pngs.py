a9f3ee58c0208a37649cfb1d282c191090b7064e
elotex0 <justin.kettner14@icloud.com>
2026-08-26 21:47:41 +0200

    Update generate_pngs.py

--- scripts/generate_pngs.py
+++ scripts/generate_pngs.py
@@ -77,31 +77,29 @@ t2m_norm = BoundaryNorm(t2m_bounds, ncolors=len(t2m_bounds))
 # ------------------------------
 # Niederschlags-Farben (tp)
 # ------------------------------
-prec_bounds = [0.1, 0.2, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+prec_bounds = [0.0, 0.1, 0.2, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
                12, 14, 16, 20, 24, 30, 40, 50, 60, 80, 100, 125]
 prec_colors = ListedColormap([
-    "#B4D7FF", "#75BAFF", "#349AFF", "#0582FF", "#0069D2",
+    "#FFFFFF", "#B4D7FF", "#75BAFF", "#349AFF", "#0582FF", "#0069D2",
     "#003680", "#148F1B", "#1ACF06", "#64ED07", "#FFF32B",
     "#E9DC01", "#F06000", "#FF7F26", "#FFA66A", "#F94E78",
     "#F71E53", "#BE0000", "#880000", "#64007F", "#C201FC",
     "#DD66FE", "#EBA6FF", "#F9E7FF", "#D4D4D4"
 ])
-prec_colors.set_under(alpha=0)
 prec_norm = mcolors.BoundaryNorm(prec_bounds, prec_colors.N)
 
 # ------------------------------
 # Aufsummierter Niederschlag (tp_acc)
 # ------------------------------
-tp_acc_bounds = [0.1, 1, 2, 3, 5, 7, 10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100,
+tp_acc_bounds = [0.0, 0.1, 1, 2, 3, 5, 7, 10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100,
                   125, 150, 175, 200, 250, 300, 400, 500]
 tp_acc_colors = ListedColormap([
-    "#B4D7FF", "#75BAFF", "#349AFF", "#0582FF", "#0069D2",
+    "#FFFFFF", "#B4D7FF", "#75BAFF", "#349AFF", "#0582FF", "#0069D2",
     "#003680", "#148F1B", "#1ACF06", "#64ED07", "#FFF32B",
     "#E9DC01", "#F06000", "#FF7F26", "#FFA66A", "#F94E78",
     "#F71E53", "#BE0000", "#880000", "#64007F", "#C201FC",
     "#DD66FE", "#EBA6FF", "#F9E7FF", "#D4D4D4", "#969696"
 ])
-tp_acc_colors.set_under(alpha=0)
 tp_acc_norm = mcolors.BoundaryNorm(tp_acc_bounds, tp_acc_colors.N)
 
 # ------------------------------
